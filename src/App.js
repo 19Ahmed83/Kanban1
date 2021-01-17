77e07feba071e1139d7b549234734f01f403a3cb
@hmed <spb6926@gmail.com>
2021-01-17 20:51:44 +0100

    repetition kanban

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import Board from "./Board";
+
+const initialTasks = [
+    {
+        id: 1,
+        title: 'Learn React',
+        status: 'todo'
+    },
+    {
+        id: 2,
+        title: 'Learn Js',
+        status: 'todo'
+    },
+    {
+        id: 3,
+        title: 'Learn Python',
+        status: 'in progress'
+    },
+    {
+        id: 4,
+        title: 'Learn Java',
+        status: 'review'
+    },
+    {
+        id: 5,
+        title: 'Learn Go',
+        status: 'done'
+    }
+]
+
+const statuses=['todo', 'in progress', 'review','done']
+const nextStatus=(status)=>statuses[statuses.indexOf(status)+1]
+const prevStatus=(status)=>statuses[statuses.indexOf(status)-1]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [tasks, setTasks] = useState(initialTasks)
+
+    const plus = (id) => {
+        const updatedTasks = tasks.map(el => el.id === id ? {...el, status:nextStatus(el.status) } : el)
+        setTasks(updatedTasks)
+    }
+
+    const minus = (id) => {
+        const updatedTasks = tasks.map(el => el.id === id ? {...el, status:prevStatus(el.status) } : el)
+        setTasks(updatedTasks)
+    }
+
+
+    return (
+        <div className="App">
+
+            <h2>Todo</h2>
+            <Board tasks={tasks} status={'todo'} plus={plus} minus={minus}/>
+
+            <h2>In Progress</h2>
+            <Board tasks={tasks} status={'in progress'} plus={plus} minus={minus}/>
+
+            <h2>Review</h2>
+            <Board tasks={tasks} status={'review'} plus={plus} minus={minus}/>
+
+            <h2>Done</h2>
+            <Board tasks={tasks} status={'done'} plus={plus} minus={minus}/>
+
+        </div>
+    );
 }
 
 export default App;
